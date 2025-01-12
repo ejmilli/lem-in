6716a281d0c03bb7ec339a53e1f944d2d95836b9
Richard-AungKhantMin <aungkhantmin.dev@gmail.com>
2025-01-12 19:34:48 +0200

    richard the first

---
+++ Tools/commandLine.go
@@ -0,0 +1,33 @@
+package Tools
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func CheckCommandLineArg() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: rooms <roomfile>")
+		os.Exit(1)
+	}
+	arg := os.Args[1]
+
+	if arg[len(arg)-4:] != ".txt" {
+		fmt.Println("Room file must end with .txt")
+		os.Exit(1)
+	}
+
+	file, err := os.ReadFile(arg)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	lines := strings.Split(string(file), "\n")
+	if len(lines) < 6 {
+		fmt.Println("Error: Room file must contain at least 6 lines")
+		os.Exit(1)
+	}
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"lemin/Tools"
+)
+
 func main() {
 
+	Tools.CheckCommandLineArg()
 }
